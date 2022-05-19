f5991f3017b6bc5226b70dbb734bd2817f5d1599
didicozaur <moisin.diana@yahoo.com>
2022-05-19 15:44:01 +0200

    added a new model

--- models/Ad.model.js
+++ models/Ad.model.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const adSchema = newSchema{
-	subject: { type: Schema.Types.ObjectId, ref: "Subject" },
-	description: String,
-	location: String,
-	user:{ type: Schema.Types.ObjectId, ref: 'User'}
-	price: {
-	  type: Number,
-	  required: true,
-	},
-	
-}
+const adSchema = new Schema({
+  title: String,
+  subject: { type: Schema.Types.ObjectId, ref: "Subject" },
+  description: String,
+  location: String,
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
 module.exports = model("Ad", adSchema);
