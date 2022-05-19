291b86e7a538d97f0a5447c09f1c48bd63913003
didicozaur <moisin.diana@yahoo.com>
2022-05-19 16:02:12 +0200

    added the functionality to read subjects

--- routes/subject.routes.js
+++ routes/subject.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { default: mongoose } = require("mongoose");
 
 const Subject = require("../models/Subject.model");
-const Teacher = require("../models/Teacher.model");
+const Ad = require("../models/Ad.model");
 
 //  POST   -  Creates a new suject
 router.post("/subjects", (req, res, next) => {
@@ -11,6 +11,41 @@ router.post("/subjects", (req, res, next) => {
   Subject.create({ title, description, level, img })
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
+});
+
+// Get list of subjects
+router.get("/subjects", (req, res, next) => {
+  Subject.find()
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      console.log("error getting list of subjects", err);
+      res.status(500).json({
+        message: "error getting list of subjects",
+        error: err,
+      });
+    });
+});
+
+//  Get details of a specific subject by id
+router.get("/subjects/:subjectId", (req, res, next) => {
+  const { subjectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(subjectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Subject.findById(subjectId)
+    .then((subject) => res.json(subject))
+    .catch((err) => {
+      console.log("error getting details of a subject", err);
+      res.status(500).json({
+        message: "error getting details of a subject",
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
